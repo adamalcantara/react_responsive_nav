54a5c872fd9b711091e22ec23ad5163eb348dae3
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-09 08:25:30 -0400

    Added menu items as ul

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,14 +3,23 @@ import { MenuItems } from "./MenuItems"
 
 class Navbar extends Component {
     render() {
-        return(
+        return (
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">React</h1>
                 <div className="menu-icon">
                     <i className=""></i>
                 </div>
                 <ul>
-                    <li><a href=""></a></li>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+
                 </ul>
             </nav>
         )
